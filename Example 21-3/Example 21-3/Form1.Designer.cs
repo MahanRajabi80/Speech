aa574b66f00ecce65a6f5756446c453c35feeb35
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 16:17:30 +0330

    Add files via upload

---
+++ Example 21-3/Example 21-3/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Example_21_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBarRate = new System.Windows.Forms.TrackBar();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSpeech = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Speech Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(319, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Speech Volume:";
+            // 
+            // trackBarRate
+            // 
+            this.trackBarRate.AutoSize = false;
+            this.trackBarRate.LargeChange = 1;
+            this.trackBarRate.Location = new System.Drawing.Point(410, 19);
+            this.trackBarRate.Minimum = -10;
+            this.trackBarRate.Name = "trackBarRate";
+            this.trackBarRate.Size = new System.Drawing.Size(191, 27);
+            this.trackBarRate.TabIndex = 2;
+            this.trackBarRate.ValueChanged += new System.EventHandler(this.trackBarRate_ValueChanged);
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.AutoSize = false;
+            this.trackBarVolume.LargeChange = 1;
+            this.trackBarVolume.Location = new System.Drawing.Point(410, 51);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(191, 27);
+            this.trackBarVolume.TabIndex = 3;
+            this.trackBarVolume.ValueChanged += new System.EventHandler(this.trackBarVolume_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Word:";
+            // 
+            // btnSpeech
+            // 
+            this.btnSpeech.Location = new System.Drawing.Point(106, 54);
+            this.btnSpeech.Name = "btnSpeech";
+            this.btnSpeech.Size = new System.Drawing.Size(121, 23);
+            this.btnSpeech.TabIndex = 1;
+            this.btnSpeech.Text = "Speech";
+            this.btnSpeech.UseVisualStyleBackColor = true;
+            this.btnSpeech.Click += new System.EventHandler(this.btnSpeech_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(106, 22);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(121, 20);
+            this.txtWord.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(408, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Slow";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(575, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fast";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(408, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 12);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Min";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(575, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Max";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSpeech;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(613, 97);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBarVolume);
+            this.Controls.Add(this.trackBarRate);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnSpeech);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example 21-3 (Speech)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBarRate;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSpeech;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Example 21-3/Example 21-3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//--------new------------------
+//Add Reference System.Speech
+using System.Speech;
+using System.Speech.Synthesis;
+//-----------------------------
+namespace Example_21_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //----------------------------------------------------------------
+        SpeechSynthesizer newSpeech = new SpeechSynthesizer();
+        //----------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            newSpeech.Rate = 0;
+            newSpeech.Volume = trackBarVolume.Value = 50;//mid
+        }
+        //----------------------------------------------------------------
+        private void btnSpeech_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtWord.Text == "")
+                    MessageBox.Show("Enter Word!!!");
+                else
+                    newSpeech.SpeakAsync(txtWord.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            txtWord.Focus();
+        }
+        //----------------------------------------------------------------
+        private void trackBarRate_ValueChanged(object sender, EventArgs e)
+        {
+            newSpeech.Rate = trackBarRate.Value;
+        }
+        //----------------------------------------------------------------
+        private void trackBarVolume_ValueChanged(object sender, EventArgs e)
+        {
+            newSpeech.Volume = trackBarVolume.Value;
+        }        
+    }
+}
